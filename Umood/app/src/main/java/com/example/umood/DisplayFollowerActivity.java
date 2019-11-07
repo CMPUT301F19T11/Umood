4444105f464e14beb9a24d475ec20b64c0a1a712
Qian Yu <529823104@qq.com>
2019-11-07 16:48:56 -0700

    .

--- Umood/app/src/main/java/com/example/umood/DisplayFollowerActivity.java
+++ Umood/app/src/main/java/com/example/umood/DisplayFollowerActivity.java
@@ -1,10 +1,14 @@
 package com.example.umood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.baoyz.swipemenulistview.SwipeMenuListView;
@@ -12,35 +16,44 @@ import com.baoyz.swipemenulistview.SwipeMenuListView;
 import java.util.ArrayList;
 
 public class DisplayFollowerActivity extends AppCompatActivity {
-    private UserAdapter adapter;
-    private ArrayList<Mood> moodList;
-    private ListView listView;
-    User user;
     private static final String TAG = "qian-follower";
-    private long click;
-    private UserList followerUserList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_display_follower);
+
+
+        // Declare:
         Intent intent = getIntent();
+        RecyclerView recyclerView;
+        UserAdapter adapter;
+        User user;
         user = (User)intent.getSerializableExtra("user");
-        followerUserList = (UserList)intent.getSerializableExtra("follower_list");
+        UserList followerUserList = (UserList)intent.getSerializableExtra("follower_list");
         Log.d(TAG, user.getUsername());
 
-        listView = findViewById(R.id.display_follower_list);
-
+        recyclerView = findViewById(R.id.recycle_view_follower);
         ArrayList<User> userList = followerUserList.getList();
+        ImageButton cancel = findViewById(R.id.imageButton);
         for(User u:userList){
             Log.d(TAG, u.getUsername());
         }
 
-        // adapter = new UserAdapter(this, R.layout.content_following_request,userList);
-        // listView.setAdapter(adapter);
+        // RecycleView Components:
 
+        GridLayoutManager layoutManager = new GridLayoutManager(this,1);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new UserAdapter(userList);
+        recyclerView.setAdapter(adapter);
 
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
