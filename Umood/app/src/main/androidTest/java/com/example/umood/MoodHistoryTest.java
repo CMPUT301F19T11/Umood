ef84f95624cfcccfa2003b4dcb3caeead36f9ad0
Yifan Yue <44985768+EcouYue@users.noreply.github.com>
2019-11-08 10:03:41 -0700

    Create MoodHistoryTest.java

---
+++ Umood/app/src/main/androidTest/java/com/example/umood/MoodHistoryTest.java
@@ -0,0 +1,55 @@
+package com.example.umood;
+
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MoodHistoryTest {
+
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MoodHistory> rule =
+            new ActivityTestRule<>(MoodHistory.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     *
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+
+
+
+    /**
+     * Closes the activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+}
+
