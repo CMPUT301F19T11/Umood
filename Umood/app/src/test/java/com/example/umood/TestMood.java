4c2d927db46c4d1f126daed30cb544ea7375495e
ArcherUz <berialyanarcheruz@outlook.com>
2019-11-08 13:46:19 -0700

    add unit tests from yifan

---
+++ Umood/app/src/test/java/com/example/umood/TestMood.java
@@ -0,0 +1,102 @@
+package com.example.umood;
+
+import com.example.umood.Mood;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ *  Test class for Mood class.
+ */
+
+public class TestMood {
+    public TestMood(){
+
+    }
+
+    @Test
+    public void testGetDate(){
+        Mood mood = new Mood();
+        mood.setDate("2019-11-07");
+        assertEquals("2019-11-07", mood.getDate());
+
+    }
+
+    @Test
+    public void testSetDate(){
+        Mood mood = new Mood();
+        mood.setDate("2019-11-07");
+        assertEquals("2019-11-07", mood.getDate());
+
+    }
+
+
+    @Test
+    public void testGetLongitude(){
+        Mood mood = new Mood();
+        mood.setLongitude(-50);
+        assertEquals(-50,mood.getLongitude());
+
+    }
+
+    @Test
+    public void testSetLongitude(){
+        Mood mood = new Mood();
+        mood.setLongitude(-50);
+        assertEquals(-50,mood.getLongitude());
+    }
+
+    @Test
+    public void testGetLatitude(){
+        Mood mood = new Mood();
+        mood.setLatitude(-50);
+        assertEquals(-50,mood.getLatitude());
+
+    }
+
+    @Test
+    public void testSetLatitude(){
+        Mood mood = new Mood();
+        mood.setLatitude(-50);
+        assertEquals(-50,mood.getLatitude());
+    }
+
+
+    @Test
+    public void testGetReason(){
+        Mood mood = new Mood();
+        mood.setReason("pass the couse");
+        assertEquals("pass the couse", mood.getReason());
+    }
+
+    @Test
+    public void testSetReason(){
+        Mood mood = new Mood();
+        mood.setReason("pass the couse");
+        assertEquals("pass the couse", mood.getReason());
+    }
+
+    @Test
+    public void testGetSocialSituation(){
+        Mood mood = new Mood();
+        mood.setSocialSituation("Alone");
+        assertEquals("Alone", mood.getSocialSituation());
+    }
+
+    @Test
+    public void testSetSocialSituation(){
+        Mood mood = new Mood();
+        mood.setSocialSituation("Alone");
+        assertEquals("Alone", mood.getSocialSituation());
+        mood.setSocialSituation("with one other person");
+        assertEquals("with one other person", mood.getSocialSituation());
+    }
+
+
+
+
+}
