b27fc2342a0018f4c1d29dfd117081c69b4f7e0e
Yifan Yue <44985768+EcouYue@users.noreply.github.com>
2019-11-17 17:39:02 -0700

    Update NotificationsFragment.java

--- Umood/app/src/main/java/com/example/umood/ui/notifications/NotificationsFragment.java
+++ Umood/app/src/main/java/com/example/umood/ui/notifications/NotificationsFragment.java
@@ -2,7 +2,6 @@ package com.example.umood.ui.notifications;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,38 +9,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.example.umood.ChartActivity;
 import com.example.umood.FeedActivity;
+import com.example.umood.LoginActivity;
 import com.example.umood.MainActivity;
-import com.example.umood.Mood;
 import com.example.umood.MoodHistory;
 import com.example.umood.MoodList;
 import com.example.umood.R;
+import com.example.umood.SettingActivity;
 import com.example.umood.User;
-import com.example.umood.addMoodInfo;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.example.umood.R.drawable;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
-    User user;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private DocumentReference docref;
-    private CollectionReference collectionReference = db.collection("users");
+    private User user;
     private static final String TAG = "qian-profile";
-    MainActivity activity;
+    private MainActivity activity;
 
-    Intent intent;
+    private Intent intent;
 
     public View onCreateView(@NonNull final LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,10 +39,10 @@ public class NotificationsFragment extends Fragment {
         activity = (MainActivity) getActivity();
         final MoodList moodEventList = activity.getMoodEventList();
         user = activity.getUser();
-        docref = collectionReference.document(user.getUsername());
 
-        ImageView imageView = root.findViewById(R.id.avater);
-        imageView.setImageResource(R.drawable.zeldaflat);
+        //ImageView imageView = root.findViewById(R.id.profile_pic);
+        ImageView imageView = root.findViewById(R.id.avatar);
+
 
 
         // Text view Setting
@@ -72,6 +60,7 @@ public class NotificationsFragment extends Fragment {
         Button chart =  root.findViewById(R.id.button_chart);
         Button feed =  root.findViewById(R.id.button_feed);
         Button setting =  root.findViewById(R.id.button_setting);
+        Button logout = root.findViewById(R.id.logout);
 
         history.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,7 +81,45 @@ public class NotificationsFragment extends Fragment {
             }
         });
 
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(activity, SettingActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        chart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(activity, ChartActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(activity, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         return root;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        activity.update();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        activity.update();
     }
 
 
